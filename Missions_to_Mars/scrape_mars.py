54f80c155264a4bdaa0f0ceb9aaa276fe71e9734
Tman <76560394+Tman2111@users.noreply.github.com>
2021-06-08 21:14:50 -0400

    fixed scrape

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -11,7 +11,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def init_browser():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
-
+    return browser
 def scrape():
     browser = init_browser()
 
